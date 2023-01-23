3e707279510edab2ef24cde07c9aae31c179860f
Askar Yalyaev <askarrussia@gmail.com>
2023-01-23 15:46:37 +0300

    migrate db settings to .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,20 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from 'src/app.controller';
 import { AppService } from 'src/app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'nest-practice',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASDWORD,
+      database: process.env.DB_NAME,
       models: [],
       autoLoadModels: true,
     }),
